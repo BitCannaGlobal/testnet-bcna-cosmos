288a7b730e3150e24b671dbedbb718f48538f79a
Raul Bernal <bernal.raul@gmail.com>
2021-03-26 18:01:07 +0100

    Update root.go

--- cmd/bcnad/cmd/root.go
+++ cmd/bcnad/cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/BitCannaGlobal/bcna/app/params"
+	"github.com/BitCannaGlobal/testnet-bcna-cosmos/app/params"
 	"github.com/cosmos/cosmos-sdk/snapshots"
 
 	"github.com/spf13/cast"
@@ -16,7 +16,7 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
-	"github.com/BitCannaGlobal/bcna/app"
+	"github.com/BitCannaGlobal/testnet-bcna-cosmos/app"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/debug"
