3d0cf196f5412e11ca445fe2761edd0c5bf1c616
Raul Bernal <bernal.raul@gmail.com>
2021-03-26 18:45:42 +0100

    paths

--- x/bcna/client/cli/query.go
+++ x/bcna/client/cli/query.go
@@ -10,7 +10,7 @@ import (
 	// "github.com/cosmos/cosmos-sdk/client/flags"
 	// sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/BitCannaGlobal/bcna/x/bcna/types"
+	"github.com/BitCannaGlobal/testnet-bcna-cosmos/x/bcna/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
